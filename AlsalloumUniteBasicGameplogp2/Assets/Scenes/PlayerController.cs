d6e868c6f37e4226b7e3177c427b3cdf50afa1ab
Josecruz777 <756199@student.dusd.net>
2025-03-12 10:01:07 -0700

    FINSHED WITH THE PROJECT

--- AlsalloumUniteBasicGameplogp2/Assets/Scenes/PlayerController.cs
+++ AlsalloumUniteBasicGameplogp2/Assets/Scenes/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class playerController : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    private float speed = 20;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
-        //we'll move the vehical fowrowd
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move the vehicl forword based on Vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car  based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
